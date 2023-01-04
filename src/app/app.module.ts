62ae83caa76d6b939799f6bc1939ce17af6689d5
Dmytro <dmytro.pavliv@lean35.net>
2023-01-04 23:05:32 +0200

    Components#1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
